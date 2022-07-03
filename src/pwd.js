367f639db5ba6521309881071c80cb3dd8fde910
vm.opensource <104187311+vm-hacker@users.noreply.github.com>
2022-07-03 10:10:07 +0530

    Update pwd.js

--- src/pwd.js
+++ src/pwd.js
@@ -1,3 +1,8 @@
+/*
+Copyright (c) 2022-2023, Venkatesh Mishra. All rights reserved
+jsutils is free and opensource software published under the GNU GPL v3 license.
+*/
+
 # Pwd (Print Working Directory)
 
 import {green} from "https://deno.land/std@0.146.0/fmt/colors.ts";
