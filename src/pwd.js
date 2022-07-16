50af8279971757fbb3c8ab2edd774a1eefe4f04f
vm.opensource <104187311+vm-hacker@users.noreply.github.com>
2022-07-16 08:03:31 +0530

    Update pwd.js

--- src/pwd.js
+++ src/pwd.js
@@ -4,7 +4,7 @@ jsutils is free and opensource software published under the GNU GPL v3 license.
 This is free software, and you are welcome to redistribute it. Jsutils comes with absoulte no warranty.
 */
 
-# Pwd (Print Working Directory)
+// Pwd (Print Working Directory)
 
 import {green} from "https://deno.land/std@0.146.0/fmt/colors.ts";
 
